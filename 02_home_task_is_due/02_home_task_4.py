b6db98401c7e7db4486b46da9a5afd40c208d817
Roman Golovko <om@bln-ua.net>
2018-08-16 22:35:21 +0300

    Initial commit
    Signed-off-by: Roman Golovko <om@bln-ua.net>

---
+++ 02_home_task_is_due/02_home_task_4.py
@@ -0,0 +1,15 @@
+#треугольник
+for i in range(10):
+    print('  ' * (10-i-1) + '* ' * (i*2+1))
+print()
+
+#Квадрат
+for i in range(10):
+    print('* ' * 10)
+print()
+
+#ступеньки
+for i in range(10):
+    print('* '*i)
+print()
+
