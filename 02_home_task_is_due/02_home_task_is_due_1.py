7fc4fca9797dbc89c9e710abae908c6e50f3570c
Roman Golovko <om@bln-ua.net>
2018-08-16 16:54:06 +0300

    Initial commit
    Signed-off-by: Roman Golovko <om@bln-ua.net>

---
+++ 02_home_task_is_due/02_home_task_is_due_1.py
@@ -0,0 +1,9 @@
+a = int(input('Enter number A: '))
+b = int(input('Enter number B: '))
+
+if a > b:
+    print('Первое больше второго числа')
+elif a < b:
+    print('Первое меньше второго числа')
+else:
+    print('Числа равны')
