99403c623c98573e64831b839be583cc1c453492
Roman Golovko <om@bln-ua.net>
2018-08-20 10:51:25 +0300

    Initial commit
    Signed-off-by: Roman Golovko <om@bln-ua.net>

--- 02_home_task_is_due/02_home_task_4.py
+++ 02_home_task_is_due/02_home_task_4.py
@@ -1,4 +1,4 @@
-#треугольник
+#Елка
 for i in range(10):
     print('  ' * (10-i-1) + '* ' * (i*2+1))
 print()
@@ -8,8 +8,20 @@ for i in range(10):
     print('* ' * 10)
 print()
 
-#ступеньки
+##треугольник
 for i in range(10):
     print('* '*i)
 print()
 
+#ромб
+n = 11
+
+print(((n - 1) * " " + "*"))
+
+for i in range(2, n + 1):
+    print(((n - i) * " " + "*" + ((i - 2) * 2 + 1) * "*" + "*"))
+
+for j in range(n - 1, 1, -1):
+    print(((n - j) * " " + "*" + ((j - 2) * 2 + 1) * "*" + "*"))
+
+print(((n - 1) * " " + "*"))
